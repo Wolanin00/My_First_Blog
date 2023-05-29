711a24a92edf946cd8b7c92b5e286392f3685adb
Mateusz Szewczyk 1 <szewczykmateusz64@gmail.com>
2023-05-29 22:33:43 +0200

    update

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ PASSWORD = 'REMOVED'
 app = Flask(__name__)
 
 # Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 ckeditor = CKEditor(app)
